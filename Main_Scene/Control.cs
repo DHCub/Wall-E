e68882aaca5adb9db91b63d4c217cb83f04127f7
DHC <dariohcvg@gmail.com>
2023-12-14 09:12:09 -0500

    funnelled Figure drawing limit update and references through GUIInterface

--- Main_Scene/Control.cs
+++ Main_Scene/Control.cs
@@ -13,12 +13,7 @@ using System.IO;
 public partial class Control : Godot.Control
 {
 	static readonly Segment S = new(new(0, 0), new(200, 200));
-	GUIInterface gsharp;
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
-
-	// Called when the node enters the scene tree for the first time.
+	public GUIInterface gsharp;
 	public override void _Ready()
 	{
 		var draw_area = GetNode<Node2D>("Draw_Area_Marg/Viewport_Container/SubViewport/Background/Node2D");
@@ -94,90 +89,9 @@ public partial class Control : Godot.Control
 		}
 
 		var txt = code.Text;
-		// old code
-		{
-			// bool hadError = false, hadRuntimeError = false;
-
-			// void ScanError(ScanError scanError)
-			// {
-			// 	ReportError(scanError.line, string.Empty, scanError.Message);
-			// }
-
-			// void RuntimeError(RuntimeError error)
-			// {
-			// 	string line = error.token?.line.ToString() ?? "unknown";
-
-			// 	console.Text += $"[line {line}] {error.Message}\n";
-			// 	hadRuntimeError = true;
-			// }
-
-			// void Error(Token token, string message)
-			// {
-			// 	if (token.type == TokenType.EOF)
-			// 	{
-			// 		ReportError(token.line, " at end", message);
-			// 	}
-			// 	else
-			// 	{
-			// 		ReportError(token.line, " at '" + token.lexeme + "'", message);
-			// 	}
-			// }
-
-			// void ReportError(int line, string where, string message)
-			// {
-			// 	Console.WriteLine($"[line {line}] Error{where}: {message}");
-			// 	hadError = true;
-			// }
-
-			// void ParseError(ParseError parseError)
-			// {
-			// 	Error(parseError.token, parseError.Message);
-			// }
-
-			// void NameResolutionError(NameResolutionError nameResolutionError)
-			// {
-			// 	Error(nameResolutionError.Token, nameResolutionError.Message);
-			// }
-
-			// void fun(string x) {
-			// 	console.Text += x + "\n";
-			// }
-
-			// Interpreter interpreter = new Interpreter(RuntimeError, fun);
-
-			// object? result = interpreter.Eval(txt, ScanError, ParseError, NameResolutionError);
-
-			// if (result != null && result != VoidObject.Void)
-			// {
-			// 	fun(result.ToString());
-			// }
-		}
-
+		
 		draw_area.Clear();
 		gsharp.Interpret(txt);
-		// Line l1 = new();
-		// Line l2 = new();		
-		// Line l3 = new();		
-		// Line l4 = new();
-
-		// Arc A1 = new();		
-		// Arc A2 = new();		
-		// Arc A3 = new();		
-		// Arc A4 = new();
-
-		// Circle C1 = new();	
-		// Circle C2 = new();	
-		// Circle C3 = new();	
-		// Circle C4 = new();	
-
-
-		// draw_area.AddDrawable(Colors.RebeccaPurple, l1, l2, l3, l4, A1, A2, A3, A4, C1, C2, C3, C4);
-
-		// ShowIntersect(X, Y, l1, l2, l3, l4, A1, A2, A3, A4, C1, C2, C3, C4);
-		// draw_area.AddDrawable(Colors.Red, l1);
-		// draw_area.QueueRedraw();
-
-		// GD.Print(txt);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
